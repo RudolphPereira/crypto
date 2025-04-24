52878593ac4bbe4ebca210b6961824bcb8a1b4a9
Rudolph Pereira <rudolph.pereira93@gmail.com>
2025-04-24 05:08:12 +0400

    fixed navigation path in layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Navigation } from "./components/Navigation/navigation";
+import { Navigation } from "./components/Navigation/Navigation";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
