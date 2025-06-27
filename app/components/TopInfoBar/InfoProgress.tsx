c63b2b91bd9fe1a54835b894a646e71693678117
Rudolph Pereira <rudolph.pereira93@gmail.com>
2025-06-27 04:15:41 +0400

    added UI topnavbar and assets

---
+++ app/components/TopInfoBar/InfoProgress.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Image from "next/image";
+import { Progress } from "@/components/ui/progress";
+
+type Props = {
+  image?: any;
+  number: string;
+  progressColor: string;
+};
+
+export const InfoProgress = ({ image, number, progressColor }: Props) => {
+  return (
+    <div className="infoProgress">
+      <div className="infoProgressBox text-xs flex items-center gap-1.5">
+        {image && (
+          <div className="iconBox w-[1.3rem] h-[1.3rem] overflow-visible">
+            <Image src={image} alt="coin icon" className="w-full h-full" />
+          </div>
+        )}
+        <div className="numBox">
+          <span>{number}</span>
+        </div>
+        <div className="progressBox">
+          <Progress
+            value={44}
+            className={`w-[3.5rem] h-[0.4rem] ${progressColor} bg-white/40 rounded-xs [&>div]:rounded-xs`}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ app/components/TopInfoBar/TopInfoBar.tsx
@@ -0,0 +1,49 @@
+import { InfoText } from "./InfoText";
+import { InfoProgress } from "./InfoProgress";
+import coinIcon from "../../assets/flash-circle.svg";
+import exchangeIcon from "../../assets/recovery-convert.svg";
+import increaseIcon from "../../assets/increase.svg";
+import bitcoinIcon from "../../assets/Currency-icon-02.svg";
+import ethIcon from "../../assets/Currency-icon-01.svg";
+
+type Props = {};
+
+export const TopInfoBar = (props: Props) => {
+  return (
+    <div className="infoBar bg-deep-plum px-2 py-3 border-b border-b-white/10 text-white flex justify-center w-full min-h-14">
+      <div className="infoBarBox flex items-center gap-5 flex-wrap justify-center">
+        <InfoText
+          image={coinIcon}
+          iconSize="w-[1.15rem] h-[1.15rem]"
+          text="Coins"
+          number={7884}
+        />
+        <InfoText
+          image={exchangeIcon}
+          iconSize="w-[1.15rem] h-[1.15rem]"
+          text="Exchange"
+          number={622}
+        />
+        <InfoText
+          image={increaseIcon}
+          iconSize="w-[0.7rem] h-[0.7rem]"
+          text="T"
+          number={1.69}
+          additionalClass="flex-row-reverse"
+        />
+
+        <InfoProgress number="$124.45B" progressColor="[&>div]:bg-white" />
+        <InfoProgress
+          image={bitcoinIcon}
+          number="44%"
+          progressColor="[&>div]:bg-orange"
+        />
+        <InfoProgress
+          image={ethIcon}
+          number="21%"
+          progressColor="[&>div]:bg-pastel-blue"
+        />
+      </div>
+    </div>
+  );
+};
