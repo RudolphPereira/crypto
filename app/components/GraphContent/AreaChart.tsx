0897c8e0cd785a3f59b9e734f9c706c5a241189f
Rudolph Pereira <rudolph.pereira93@gmail.com>
2025-07-13 12:35:36 +0400

    added tooltip style fix on graphs

--- app/components/GraphContent/AreaChart.tsx
+++ app/components/GraphContent/AreaChart.tsx
@@ -64,7 +64,7 @@ export function ChartAreaGradient() {
         >
           <ChartTooltip
             cursor={false}
-            content={<ChartTooltipContent hideIndicator />}
+            content={<ChartTooltipContent className="rounded-sm" />}
           />
           <defs>
             <linearGradient id="fillCoinOne" x1="0" y1="0" x2="0" y2="1">
