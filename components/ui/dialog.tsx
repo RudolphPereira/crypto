7e10aebb0b18d93d03475402a1828f78e8d3df29
Rudolph Pereira <rudolph.pereira93@gmail.com>
2025-07-20 10:54:15 +0400

    removed focus-ring on popup

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -69,9 +69,9 @@ function DialogContent({
         {showCloseButton && (
           <DialogPrimitive.Close
             data-slot="dialog-close"
-            className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4"
+            className="ring-offset-background cursor-pointer  data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100  disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4"
           >
-            <CircleX className="text-background cursor-pointer size-6 outline-0 focus:ring-0" />
+            <CircleX className="text-background size-6" />
             <span className="sr-only ">Close</span>
           </DialogPrimitive.Close>
         )}
